7ebd3673d9e7eb8fb3d36d9f16f8cd81a42b932c
Lio3307 <aureliofs01@gmail.com>
2025-08-10 16:17:58 +0800

    change style

--- src/components/layouts/AboutUs.tsx
+++ src/components/layouts/AboutUs.tsx
@@ -11,7 +11,7 @@ const AboutUs = () => {
             />
           </div>
           <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-black">
               Before they sold out
               <br className="hidden lg:inline-block" />
               readymade gluten
@@ -23,10 +23,7 @@ const AboutUs = () => {
               truffaut hexagon try-hard chambray.
             </p>
             <div className="flex justify-center">
-              <button className="inline-flex text-white bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-600 rounded text-lg">
-                Button
-              </button>
-              <button className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+              <button className="inline-flex text-black hover:text-white hover:cursor-pointer outline-2 outline-sky-500 bg-transparent border-0 py-2 px-[2.7rem] focus:outline-none hover:bg-sky-500 rounded text-lg">
                 Button
               </button>
             </div>
