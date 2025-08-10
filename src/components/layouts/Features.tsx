0ede467bc775e6e626f22d2fd9b6a38f55186fd2
Lio3307 <aureliofs01@gmail.com>
2025-08-10 16:28:23 +0800

    change background color

--- src/components/layouts/Features.tsx
+++ src/components/layouts/Features.tsx
@@ -1,7 +1,7 @@
 const Features = () => {
     return (
         <>
-        <div className="bg-gray-50">
+        <div className="bg-white">
     <section className="py-16 px-4 sm:px-6 lg:px-8">
         <div className="max-w-7xl mx-auto">
             <div className="text-center mb-12">
