9d34f3d784af9acfc24cda7d0c28d7b85c0a24ac
Lio3307 <aureliofs01@gmail.com>
2025-08-11 14:46:22 +0800

    adding dashboard comonent

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import { Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import SignUp from "./components/layouts/auth/SignUp"
-import SignIn from "./components/layouts/auth/SignIn"
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./components/layouts/auth/SignUp";
+import SignIn from "./components/layouts/auth/SignIn";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/signUp" element={<SignUp/>}/>
-        <Route path="/signIn" element={<SignIn/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,13 @@
+import Sidebar from "../components/layouts/dashboard/Sidebar";
+
+const Dashboard = () => {
+  return (
+    <>
+      <Sidebar>
+        <h2>Hello</h2>
+      </Sidebar>
+    </>
+  );
+};
+
+export default Dashboard;
