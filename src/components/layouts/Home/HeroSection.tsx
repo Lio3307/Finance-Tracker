b69172b865ff14ae3eaa713f539668d1a2e2127d
Lio3307 <aureliofs01@gmail.com>
2025-08-11 13:49:32 +0800

    change context and adding link

--- src/components/layouts/Home/HeroSection.tsx
+++ src/components/layouts/Home/HeroSection.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const HeroSection = () => {
   return (
     <>
@@ -5,28 +7,20 @@ const HeroSection = () => {
         <div className="relative isolate px-6 lg:px-8">
           <div className="mx-auto max-w-2xl py-[6rem] text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Build your digital presence with confidence
+              Manage your finances with confidence
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              We create stunning websites and applications that help your
-              business grow faster and smarter.
+              Track expenses, set budgets, and achieve your goals with a smart
+              and easy-to-use financial tool.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
-                className="rounded-md bg-indigo-600 px-4 py-2 text-sm font-semibold text-white shadow hover:bg-indigo-500"
-              >
-                Get Started
-              </a>
-              <a href="#" className="text-sm font-semibold text-gray-900">
-                Learn More →
-              </a>
+              <Link to={"/signUp"} className="rounded-md bg-indigo-600 px-4 py-2 text-sm font-semibold text-white shadow hover:bg-indigo-500">
+              Get Started →
+              </Link>
             </div>
           </div>
           <div className="absolute inset-x-0 top-[calc(100%-10rem)] -z-10 overflow-hidden blur-3xl">
-            <div className="w-[72rem] h-[150rem] mx-auto bg-gradient-to-tr from-pink-300 via-indigo-300 to-purple-300 opacity-30 rounded-full">
-                
-            </div>
+            <div className="w-[72rem] h-[150rem] mx-auto bg-gradient-to-tr from-pink-300 via-indigo-300 to-purple-300 opacity-30 rounded-full"></div>
           </div>
         </div>
       </section>
