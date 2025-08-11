f26c0011c1dd7e6e0ade1761adda616524014aca
Lio3307 <aureliofs01@gmail.com>
2025-08-11 12:45:16 +0800

    adding Footer component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,7 @@ import AboutUs from "../components/layouts/AboutUs";
 import Features from "../components/layouts/Features";
 import Testimonial from "../components/layouts/Testimonial";
 import Faq from "../components/layouts/Faq";
+import Footer from "../components/layouts/Footer";
 
 const Home = () => {
   return (
@@ -14,6 +15,7 @@ const Home = () => {
       <Features />
       <Testimonial/>
       <Faq/>
+      <Footer/>
     </>
   );
 };
