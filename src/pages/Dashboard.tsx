b47fe9dcc1386905ae4473611d31c714980e2829
Lio3307 <aureliofs01@gmail.com>
2025-08-13 18:17:37 +0800

    adding in auth state

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,26 @@
 import Sidebar from "../components/layouts/dashboard/Sidebar";
 import { Outlet, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { auth } from "../config/firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 const Dashboard = () => {
   const location = useLocation();
   const pathValue: string = location.pathname.split("/")[2] || "home";
+
+    useEffect(() => {
+    const unsubs = onAuthStateChanged(auth, async (user) => {
+      if (!user) {
+        alert("you have to register first!!")
+        window.location.href = "/";
+        return;
+      }else {
+        return;
+      }
+    });
+
+    return () => unsubs();
+  }, []);
   return (
     <>
       <Sidebar getPath={pathValue}>
