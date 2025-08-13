6f14d36fc13996f46688280f6d08c7ba3b3cae8f
Lio3307 <aureliofs01@gmail.com>
2025-08-13 16:16:58 +0800

    cerate signIn and signUp function

---
+++ src/state/useAuth.js
@@ -0,0 +1,32 @@
+import { createUserWithEmailAndPassword } from "firebase/auth/cordova";
+import { create } from "zustand";
+import { doc, setDoc } from "firebase/firestore";
+import loading from "daisyui/components/loading";
+
+const useAuth = create((set) => ({
+  loading: false,
+  signUpWithEmail: async function (auth, db, email, password, fullName) {
+    try {
+      set({ loading: true });
+      await createUserWithEmailAndPassword(auth, email, password);
+      const docRef = doc(db, "Users", auth?.currentUser.id);
+      await setDoc(docRef, {
+        fullName,
+        email,
+        userId: auth?.currentUser.id,
+      });
+      set({ loading: false });
+    } catch (err) {
+      throw new Error("Cannot create user account! : " + err);
+    }
+  },
+  signInWithEmail: async function (auth, email, password) {
+    try {
+      set({ loading: true });
+      await signInWithEmailAndPassword(auth, email, password);
+      set({ loading: false });
+    } catch (err) {
+      throw new Error("Cannot sign In : " + error);
+    }
+  },
+}));
