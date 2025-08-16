fafa9ca3cbf1f197bccfdfd08c0594c6f9904f05
Lio3307 <aureliofs01@gmail.com>
2025-08-16 18:33:50 +0800

    adding sign out handler

--- src/components/layouts/dashboard/Sidebar.tsx
+++ src/components/layouts/dashboard/Sidebar.tsx
@@ -1,5 +1,7 @@
+import { signOut } from "firebase/auth";
 import { useState, type ReactNode } from "react";
 import { Link } from "react-router-dom";
+import { auth } from "../../../config/firebase";
 
 type Props = {
   getPath: string;
@@ -14,9 +16,22 @@ export default function Sidebar({ children, getPath }: Props) {
     { name: "Target", to: "target" },
   ];
 
+  const handleSignOut = async () => {
+    const confirmSignOut = confirm("You sure want to sign out?")
+    if(!confirmSignOut)return;
+    try {
+      await signOut(auth)
+      alert("Successfully sign out")
+      window.location.href = "/"
+    } catch (err) {
+      throw new Error(`Error : ${err}`);
+      
+    }
+
+  }
+
   return (
-    <div className="h-screen bg-white text-gray-800 flex flex-col md:flex-row">
-      {/* Mobile Top Bar */}
+    <div className="h-screen bg-gray-100 text-gray-800 flex flex-col md:flex-row">
       <button
         onClick={() => setOpen(true)}
         aria-label="Open Sidebar"
@@ -38,7 +53,7 @@ export default function Sidebar({ children, getPath }: Props) {
       <aside
         className={`fixed md:sticky top-0 left-0 h-screen w-64 bg-white border-r border-gray-100 shadow-xl z-50 transform transition-transform duration-300 ease-in-out overflow-y-auto ${
           open ? "translate-x-0" : "-translate-x-full"
-        } md:translate-x-0`}
+        } md:translate-x-0 flex flex-col`}
       >
         {/* Sidebar Header */}
         <div className="p-4 flex items-center justify-between border-b border-gray-100">
@@ -57,23 +72,35 @@ export default function Sidebar({ children, getPath }: Props) {
           </button>
         </div>
 
-        {/* Navigation */}
-        <nav className="p-4 space-y-1">
-          {navItems.map((t, i) => (
-            <Link
-              key={i}
-              to={`/dashboard/${t.to}`}
-              onClick={() => setOpen(false)}
-              className={`flex items-center px-3 py-2 rounded-md transition-all duration-200 ${
-                getPath === t.to
-                  ? "bg-indigo-50 text-indigo-600 font-medium"
-                  : "text-gray-700 hover:bg-indigo-50 hover:text-indigo-600"
-              }`}
-            >
-              {t.name}
-            </Link>
-          ))}
-        </nav>
+        <div className="flex-1 flex flex-col justify-between">
+          <nav className="p-4 space-y-1">
+            {navItems.map((t, i) => (
+              <Link
+                key={i}
+                to={`/dashboard/${t.to}`}
+                onClick={() => setOpen(false)}
+                className={`flex items-center px-3 py-2 rounded-md transition-all duration-200 ${
+                  getPath === t.to
+                    ? "bg-indigo-50 text-indigo-600 font-medium"
+                    : "text-gray-700 hover:bg-indigo-50 hover:text-indigo-600"
+                }`}
+              >
+                {t.name}
+              </Link>
+            ))}
+          </nav>
+          <div className="p-4 border-t border-gray-100">
+            <button 
+            onClick={(e) => {
+              e.preventDefault()
+              e.stopPropagation()
+              handleSignOut()
+            }}
+            className="w-full px-3 py-2 rounded-md bg-red-100 text-red-600 font-medium cursor-pointer hover:bg-red-200 transition">
+              Sign Out
+            </button>
+          </div>
+        </div>
       </aside>
 
       {/* Main Content */}
