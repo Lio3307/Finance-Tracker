ac016f2ad5888690dcc1236b11746dfc99ef2c4c
Lio3307 <aureliofs01@gmail.com>
2025-08-17 18:16:13 +0800

    adding margin tpp loading in fallback

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -32,7 +32,7 @@ const Home = () => {
     <>
       <Suspense
         fallback={
-          <div className="flex justify-center items-center">
+          <div className="flex mt-[14rem] justify-center items-center">
             <div className="animate-spin rounded-full h-12 w-12 border-4 border-blue-500 border-t-transparent"></div>
           </div>
         }
