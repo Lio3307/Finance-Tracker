db9bbcca461fbdcd38b6508a84d03468bebbc563
Lio3307 <aureliofs01@gmail.com>
2025-08-17 16:29:55 +0800

    adding hooks for mobile animation

--- src/components/layouts/Home/AboutUs.tsx
+++ src/components/layouts/Home/AboutUs.tsx
@@ -1,21 +1,23 @@
 import { Link } from "react-router-dom";
 import { motion } from "motion/react";
+import useIsMobile from "../../../hooks/useIsMobile";
 
 const AboutUs = () => {
+  const isMobile = useIsMobile();
   return (
     <>
       <section className="text-gray-400 bg-transparent relative z-30 body-font">
         <div className="container mx-auto flex px-5 py-[8rem] md:flex-row flex-col items-center">
           <motion.div
-            initial={{ opacity: 0, x: -50 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={{
-              duration: 0.3,
-              ease: "easeInOut",
-              delay: 0.2,
+            initial={{
+              opacity: 0,
+              x: isMobile ? 0 : -50,
+              y: isMobile ? 50 : 0,
             }}
+            whileInView={{ opacity: 1, x: 0, y: 0 }}
+            transition={{ duration: 0.4, ease: "easeOut" }}
           >
-            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 md:mb-0 mb-10">
+            <div className="md:mb-0 mb-10">
               <img
                 className="object-cover object-center rounded"
                 alt="hero"
@@ -24,47 +26,43 @@ const AboutUs = () => {
               />
             </div>
           </motion.div>
+
+          {/* Teks */}
           <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
             <motion.div
-              initial={{ opacity: 0, y: -50 }}
+              initial={{ opacity: 0, y: 50 }}
               whileInView={{ opacity: 1, y: 0 }}
-              transition={{
-                duration: 0.3,
-                ease: "easeInOut",
-                delay: 0.2,
-              }}
+              transition={{ duration: 0.4, ease: "easeOut", delay: 0.1 }}
+              viewport={{ once: true }}
             >
               <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-black">
-                Before they sold out
-                <br className="hidden lg:inline-block" />
-                readymade gluten
+                Manage Your Finance <br className="hidden lg:inline-block" />
+                Smarter and Faster
               </h1>
             </motion.div>
+
             <motion.div
-              initial={{ opacity: 0, x: 50 }}
-              whileInView={{ opacity: 1, x: 0 }}
-              transition={{
-                duration: 0.3,
-                ease: "easeInOut",
-                delay: 0.2,
+              initial={{
+                opacity: 0,
+                x: isMobile ? 0 : 50,
+                y: isMobile ? 50 : 0,
               }}
+              whileInView={{ opacity: 1, x: 0, y: 0 }}
+              transition={{ duration: 0.4, ease: "easeOut", delay: 0.2 }}
+              viewport={{ once: true }}
             >
               <p className="mb-8 leading-relaxed">
-                Copper mug try-hard pitchfork pour-over freegan heirloom neutra
-                air plant cold-pressed tacos poke beard tote bag. Heirloom echo
-                park mlkshk tote bag selvage hot chicken authentic tumeric
-                truffaut hexagon try-hard chambray.
+                Track expenses, set budgets, and get real-time insights to stay
+                on top of your financial goals.
               </p>
             </motion.div>
+
             <div className="flex justify-center">
               <motion.div
                 initial={{ opacity: 0, y: 50 }}
                 whileInView={{ opacity: 1, y: 0 }}
-                transition={{
-                  duration: 0.3,
-                  ease: "easeInOut",
-                  delay: 0.2,
-                }}
+                transition={{ duration: 0.4, ease: "easeOut", delay: 0.3 }}
+                viewport={{ once: true }}
               >
                 <Link
                   to={"/auth/signUp"}
