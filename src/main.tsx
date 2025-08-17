7b9fb38ef3e4e1c0ecb130aa63a5f4f69b5388f1
Lio3307 <aureliofs01@gmail.com>
2025-08-17 18:01:11 +0800

    change browserRouter to hashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { QueryClient,QueryClientProvider } from "@tanstack/react-query";
+import { HashRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import App from "./App.tsx";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-      <App /></QueryClientProvider>
-    </BrowserRouter>
+    <HashRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </HashRouter>
   </StrictMode>
 );
